f029c1e726a064ff4861ccb6aafe1c0a84a9a616
jpass23 <jpass23@live.unc.edu>
2022-04-19 16:35:55 -0400

    Merge branch 'main' of https://github.com/comp426-2022-spring/a04-jpass23

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 //FUNCTIONS//
-//comment
 function coinFlip() {
     return Math.floor(Math.random()*2) ? 'heads' : 'tails';
 }
@@ -60,11 +59,7 @@ app.use(morgan('tiny'))
 //require fs:
 const fs = require('fs')
 
-//require better-sqlite3:
-const Database = require('better-sqlite3');
-
-// Connect to a database or create one if it doesn't exist yet.
-const db = new Database('log.db');
+const db = require('./database.js')
 
 const help = (`
 server.js [options]
@@ -95,57 +90,78 @@ var LOG = args['log'] || true
 var HELP = args['help'] || false
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Start an app server
+// Server port
+var HTTP_PORT = 5000 
+// Start server
 const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+// READ (HTTP method GET) at root endpoint /app/
+app.get("/app/", (req, res, next) => {
+    res.json({"message":"Your API works! (200)"});
+	res.status(200);
 });
 
-// Setting Endpoint
-app.get('/app/', (req, res) => {
-// Respond with status 200
-    res.statusCode = 200;
-// Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+ ' ' +res.statusMessage)
-});
-
-app.get('/app/flip/', (req, res) => {
-    res.status(200).type('text/json');
-	res.json({flip: coinFlip()})
-});
-
-app.get('/app/flips/:number', (req, res) => {
-    res.status(200).type('text/json');
-    var x = coinFlips(isFinite(req.params.number)?req.params.number:1);
-    const flips = {
-        raw: x,
-        summary: countFlips(x),
+// Define other CRUD API endpoints using express.js and better-sqlite3
+// CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new/user", (req, res, next) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
     }
-	res.json(flips)
+    const stmt = db.prepare('INSERT INTO userinfo (username, password) VALUES (?, ?)')
+    const info = stmt.run(data.user, data.pass)
+    res.status(200).json(info)
+});
+// READ a list of users (HTTP method GET) at endpoint /app/users/
+app.get("/app/users", (req, res) => {	
+    try {
+        const stmt = db.prepare('SELECT * FROM userinfo').all()
+        res.status(200).json(stmt)
+    } catch {
+        console.error(e)
+    }
 });
 
-// app.get('/app/flip/call/heads', (req, res) => {
-//     res.status(200).type('text/json');
-// 	res.json(flipACoin('heads'))
-// });
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM userinfo WHERE id = ?').get(req.params.id);
+        res.status(200).json(stmt)
+    } catch (e) {
+        console.error(e)
+    }
 
-// app.get('/app/flip/call/tails', (req, res) => {
-//     res.status(200).type('text/json');
-// 	res.json(flipACoin('tails'))
-// });
-
-
-app.get('/app/flip/call/:guess(heads|tails)', (req, res) => {
-    res.status(200).type('text/json');
-	res.json(flipACoin(req.params.guess))
 });
 
+// UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+    let data = {
+        user: req.body.username,
+        pass: req.body.password
+    }
+    const stmt = db.prepare('UPDATE userinfo SET username = COALESCE(?,username), password = COALESCE(?,password) WHERE id = ?')
+    const info = stmt.run(data.user, data.pass, req.params.id)
+    res.status(200).json(info)
+});
 
-
+// DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+    const stmt = db.prepare('DELETE FROM userinfo WHERE id = ?')
+    const info = stmt.run(req.params.id)
+    res.status(200).json(info)
+});
 // Default response for any other request
 app.use(function(req, res){
-    res.status(404).type("text/plain")
-    res.send('404 NOT FOUND')    
+	res.json({"message":"Endpoint not found. (404)"});
+    res.status(404);
 });
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Server stopped')
+    })
+})
