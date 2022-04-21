44092cf0658624fbc8656e359638ae3b29ab6f00
jpass23 <jpass23@live.unc.edu>
2022-04-21 12:57:52 -0400

    Lets try again

--- database.js
+++ database.js
@@ -17,8 +17,7 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
-        INSERT INTO accesslog (username, password) VALUES ('user1','supersecurepassword'),('test','anotherpassword');
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT,remoteuser TEXT,time TEXT,method TEXT,url TEXT,protocol TEXT,httpversion TEXT,status TEXT,referer TEXT,useragent TEXT );
     `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
